4c60a3e87bd97af7bd9174d060cd6ebd47542d2f
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-10-07 18:55:04 +0400

    repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  if (num < 0) return 'ERROR';
+  let repeatedStr = '';
+  for (let i = 1; i <= num; i++) {
+    repeatedStr += str;
+  }
+  return repeatedStr;
 };
-
+//console.log(repeatString('hey', 1));
 // Do not edit below this line
 module.exports = repeatString;

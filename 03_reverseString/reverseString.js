e60610bfb2f4abd43bf1a7cbadb8b7fdc55cdc26
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-10-08 06:33:20 +0400

    reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
+const reverseString = function (text) {
+  const arrayOfChar = text.split('');
 
+  return arrayOfChar.reverse().join('');
 };
 
+//console.log(reverseString('hello'));
 // Do not edit below this line
 module.exports = reverseString;

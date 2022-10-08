3fd7a2e8f24bd24416e70624be695a385c542920
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-10-08 07:23:23 +0400

    remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+  //   for (const arg of args) {
+  //     if (arr.includes(arg)) {
+  //       arr.splice(arr.indexOf(arg), 1);
+  //     }
+  //   }
+  //   return arr;
+  return arr.filter((el) => !args.includes(el));
 };
 
 // Do not edit below this line

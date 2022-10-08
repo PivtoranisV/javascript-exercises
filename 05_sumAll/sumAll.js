6f2a91d38600a134078e6cb013acba49f692d0ef
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-10-08 20:40:08 +0400

    sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,53 @@
-const sumAll = function() {
+const sumAll = function (first, last) {
+  //   if (
+  //     first < 0 ||
+  //     last < 0 ||
+  //     typeof first !== 'number' ||
+  //     typeof last !== 'number'
+  //   ) {
+  //     return 'ERROR';
+  //   }
 
+  //   let arrayOfAllNumbers = [];
+  //   if (first < last) {
+  //     for (let i = first; i <= last; i++) {
+  //       arrayOfAllNumbers.push(i);
+  //     }
+  //   } else {
+  //     for (let i = last; i <= first; i++) {
+  //       arrayOfAllNumbers.push(i);
+  //     }
+  //   }
+
+  //   let sum = 0;
+  //   for (let i = 0; i < arrayOfAllNumbers.length; i++) {
+  //     sum += arrayOfAllNumbers[i];
+  //   }
+  //   return sum;
+  if (
+    first < 0 ||
+    last < 0 ||
+    typeof first !== 'number' ||
+    typeof last !== 'number'
+  ) {
+    return 'ERROR';
+  }
+
+  let arrayLength = 0;
+  let arrayOfAllNumbers;
+  if (last > first) {
+    arrayLength = last - first + 1;
+    arrayOfAllNumbers = Array(arrayLength)
+      .fill(first)
+      .map((el, index) => el + index);
+  } else {
+    arrayLength = first - last + 1;
+    arrayOfAllNumbers = Array(arrayLength)
+      .fill(last)
+      .map((el, index) => el + index);
+  }
+
+  return arrayOfAllNumbers.reduce((prev, next) => prev + next);
 };
 
 // Do not edit below this line

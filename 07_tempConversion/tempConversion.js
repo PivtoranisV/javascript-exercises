22062d9e3b54042d6ed2379077596e00f412b664
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-10-10 23:55:21 +0400

    temp Conversation

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+  const celsius = (fahrenheit - 32) * (5 / 9);
+  return celsius % 1 === 0 ? celsius : +celsius.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+  const fahrenheit = celsius * (9 / 5) + 32;
+  return fahrenheit % 1 === 0 ? fahrenheit : +fahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
